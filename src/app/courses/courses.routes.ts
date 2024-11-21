586e57273033fe13d63f8a3977021faf1e87a6c1
Gabriela Pimentel <78750796+gabifp@users.noreply.github.com>
2024-11-21 16:20:18 -0300

    Inclui configuracao de rota do courses

---
+++ src/app/courses/courses.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { CoursesComponent } from './containers/courses/courses.component';
+
+export const COURSES_ROUTER: Routes = [
+  {
+    path: '',
+    component: CoursesComponent
+  }
+]

9d55488a2a869637798b07238ffc1eb4f89aeef7
Gabriela Pimentel <78750796+gabifp@users.noreply.github.com>
2024-11-28 20:52:11 -0300

    Inclui variaveis e modulo da tabela do material

--- src/app/courses/containers/courses/courses.component.ts
+++ src/app/courses/containers/courses/courses.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { Course } from '../../model/course';
+
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+  courses: Course[] = [];
+  displayedColumns = ['name', 'category'];
 
 }
